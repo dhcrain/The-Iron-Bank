dfdf72d92a2958b6c2b42d1cf90bc90600f19262
dhcrain <dhcrain@gmail.com>
2016-06-21 13:27:58 -0400

    simplfied views code based on todays lecture

--- bank_app/views.py
+++ bank_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 from bank_app.models import Transaction
 from django.http import request
 from django.contrib.auth.models import User
@@ -37,22 +37,17 @@ class AccountView(LoginRequiredMixin, ListView):
         get_balance(self)
         context = super().get_context_data(**kwargs)
         context["balance"] = self.acct_balance
-        context['transactions'] = Transaction.objects.filter(user=self.request.user).filter(date__lte=datetime.datetime.today(), date__gt=datetime.datetime.today()-datetime.timedelta(days=30))
+        thirty_days_ago = datetime.datetime.now() + datetime.timedelta(days=-30)
+        context['transactions'] = Transaction.objects.filter(user=self.request.user).filter(date__gt=thirty_days_ago)
         return context
 
-class DetailView(LoginRequiredMixin, TemplateView):
+class TransactionDetailView(LoginRequiredMixin, DetailView):
     model = Transaction
     template_name = 'transaction_detail.html'
 
-    def get_context_data(self, **kwargs):
-        trans_pk = self.kwargs.get('pk', None)
-        trans = Transaction.objects.get(id=trans_pk)
-        context = super().get_context_data(**kwargs)    # I have no idea what this does
-        if self.request.user == trans.user:
-            context["transaction"] = trans
-        else:
-            context["not_auth"] = "You are not authorized to view this data."
-        return context
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+
 
 # can't navigate to, will impliment in part 2, cant get the <0 finction to work, and its late.
 class AddTransactionView(CreateView):
